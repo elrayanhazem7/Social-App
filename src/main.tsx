b7565e9424b3ef0ff5602b83057f6db5e73ce864
Z00z <elrayanhazem7@gmail.com>
2026-09-06 12:35:03 +0300

    update project

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "../src/index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./Routing/AppRouter/AppRouter";
 import { Toaster } from "react-hot-toast";
